0eccbd0f9535e8a0745be8c8d26d22ad3ed1aa27
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-03-30 20:33:31 +0200

    11

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,23 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            int betterStudents = 0;
+            for(int i = 0; i < Students.Count; i++)
+            {
+                if (Students[i].AverageGrade > averageGrade)
+                    betterStudents++;
+            }
+            double ranking = (double)(betterStudents + 1) / Students.Count;
+
+            if (ranking <= .2) return 'A';
+            if (ranking <= .4) return 'B';
+            if (ranking <= .6) return 'C';
+            if (ranking <= .8) return 'D';
+            else return 'F';
+        }
     }
 }
